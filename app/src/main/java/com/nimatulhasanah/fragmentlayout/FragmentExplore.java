c495fc18cfad168b8e24c6a88db766964f040400
fikran-dev <fikranwaleng@gmail.com>
2020-04-27 16:34:27 +0800

    Initial commit

---
+++ app/src/main/java/com/nimatulhasanah/fragmentlayout/FragmentExplore.java
@@ -0,0 +1,20 @@
+package com.nimatulhasanah.fragmentlayout;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentExplore extends Fragment {
+    View view;
+    public FragmentExplore() {
+    }
+
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.explore_fragment,container,false);
+        return view;
+    }
+}
